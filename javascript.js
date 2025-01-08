fe71a29448517003f1b099d6a6d6601d312af260
Cocaineairdrop <madonna.s.murphy@gmail.com>
2025-01-07 23:25:01 -0800

    Did exercises 1-3

--- javascript.js
+++ javascript.js
@@ -5,4 +5,11 @@ let d = 4;
 let e = 5;
 let f = 6;
 
-console.log(a + b);
+//1. Add 2 numbers together//
+console.log("1. ", a + b);
+
+//2. Add a sequence of 6 different numbers//
+console.log("2. ", a + b + c + d + e + f);
+
+//Print value of (4 + 6 + 9) / 77
+console.log("3. ", (4 + 6 + 9)/77);
