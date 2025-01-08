60147c212fbdf1b71e187dd73357eb1f150a7e6a
Cocaineairdrop <madonna.s.murphy@gmail.com>
2025-01-07 23:50:44 -0800

    Finished these exercises

--- javascript.js
+++ javascript.js
@@ -1,15 +1,24 @@
-let a = 1;
-let b = 2;
-let c = 3;
-let d = 4;
-let e = 5;
-let f = 6;
+let a = 10;
+
 
 //1. Add 2 numbers together//
-console.log("1. ", a + b);
+console.log("1. ", 2 + 3);
 
 //2. Add a sequence of 6 different numbers//
-console.log("2. ", a + b + c + d + e + f);
+console.log("2. ", 1 + 2 + 3 + 4 + 5 + 6);
 
-//Print value of (4 + 6 + 9) / 77
+//3. Print value of (4 + 6 + 9) / 77 //
 console.log("3. ", (4 + 6 + 9)/77);
+
+//4. Try the following in the console: 9 * a
+//Now try typing this in the console: let b = 7 * a 
+// (returns undefined *) and then console.log(b)
+console.log("4a. ",9 * a);
+let b = 7 * a;
+console.log("4b. ",b);
+
+//5. Try this sequence//
+const max = 57;
+let actual = max - 13;
+let percentage = actual/max;
+console.log("5. ",percentage);
